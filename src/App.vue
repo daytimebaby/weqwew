b52ed0bba067d6e139d62ce6379ef2730a2f7f75
todaybaby <1548051621@qq.com>
2022-08-27 19:19:33 +0800

    dsfsf

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+//import home from './pages/home'
+export default {
+  name: 'app',
+  components: {
+    //home
+  },
+  data(){
+    return {
+      
+    }
+  },
+  mounted(){
+    if(this.$cookie.get('userId')){
+      this.getUser();
+      this.getCartCount();
+    }
+  },
+  methods:{
+    getUser(){
+      this.axios.get('/user').then((res={})=>{
+        this.$store.dispatch('saveUserName',res.username);
+      })
+    },
+    getCartCount(){
+      this.axios.get('/carts/products/sum').then((res=0)=>{
+        this.$store.dispatch('saveCartCount',res);
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/reset.scss';
+@import './assets/scss/config.scss';
+@import './assets/scss/button.scss';
+</style>
